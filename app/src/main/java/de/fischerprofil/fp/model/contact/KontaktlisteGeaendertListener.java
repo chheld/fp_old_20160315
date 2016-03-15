1d272e72885b6df1cb3388d0c5769b1e088ea178
held <christoph.held@gmx.de>
2016-03-15 08:41:47 +0100

    AI-143.2664576 <held@PC-Held Overwrite local to https://github.com/chheld/fp.git

--- app/src/main/java/de/fischerprofil/fp/model/contact/KontaktlisteGeaendertListener.java
+++
@@ -1,7 +0,0 @@
-package de.fischerprofil.fp.model.contact;
-
-import java.util.EventListener;
-
-public interface KontaktlisteGeaendertListener extends EventListener{
-    public  void onKontaktlisteGeaendert(String s);
-}
--- app/src/main/java/de/fischerprofil/fp/model/order/AuftragslisteGeaendertListener.java
+++
@@ -1,7 +0,0 @@
-package de.fischerprofil.fp.model.order;
-
-import java.util.EventListener;
-
-public interface AuftragslisteGeaendertListener extends EventListener{
-    public  void onAuftragslisteGeaendert(String l);
-}
